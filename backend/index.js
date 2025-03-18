a37cf8a44c44674a9c7c8f6e8a74e953054ff32a
shambu2 <mugalishambu601@gmail.com>
2025-03-19 02:18:08 +0530

    added functionality to place form and created backend route for image upload

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,19 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { image } from "image-downloader"
+import { fileURLToPath } from 'url';
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 configDotenv();
-// const bcryptSalt = bcrypt.genSaltSync(10)
+
 const secret_key = "adf;;lsdkfjadskjf";
 const app = express();
+app.use('/uploads',express.static(__dirname + '/uploads'))
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin: "http://localhost:5173",
   credentials: true,
 }));
 app.use(cookieParser());
@@ -62,13 +68,13 @@ app.post("/login", async (req, res) => {
     res.status(400).json({ message: "error while logging" });
   }
 });
-app.post("/logout",async(req,res) =>{
-  res.cookie('token','').json(true);
+app.post("/logout", async (req, res) => {
+  res.cookie('token', '').json(true);
 })
-app.get("/profile", async(req, res) => {
+app.get("/profile", async (req, res) => {
   const { token } = req.cookies;
   if (token) {
-    jwt.verify(token, secret_key, {}, async(err, userInfo) => {
+    jwt.verify(token, secret_key, {}, async (err, userInfo) => {
       if (err) throw err;
       const userDoc = await User.findById(userInfo.id)
       res.json(userDoc);
@@ -77,6 +83,23 @@ app.get("/profile", async(req, res) => {
     res.json(null);
   }
 });
+
+app.post('/upload-by-link', async (req, res) => {
+  const { link } = req.body;
+  const newName = 'photo' + Date.now() + '.jpg';
+  const options = {
+    url: link,
+    dest: __dirname + '/uploads/' + newName, // will be saved to /path/to/dest/image.jpg
+  };
+  try {
+    await image(options);
+    res.json(newName)
+  } catch (error) {
+    res.json(error)
+  }
+
+})
+
 
 app.listen(process.env.PORT, () => {
   mongoose.connect(process.env.mongo_db);
--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -15,7 +15,7 @@ export  function UserContextProvider({ children }) {
         setUser(data);
         setReady(true)
       });
-    }
+    } 
   },[]);
   return (
     <UserContext.Provider value={{ user, setUser,ready }}>
